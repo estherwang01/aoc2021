98ab5b7238020fad58e4c79447ba3f2b1bb1d9e8
Esther Wang <ew375@cornell.edu>
2022-12-04 00:16:09 -0500

    js

---
+++ Solutions/src/year2022/Day4.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const internal = require('stream');
+const allContents = fs.readFileSync('day4input.txt', 'utf-8');
+var total = 0 
+allContents.split(/\r?\n/).forEach((line) => {
+    var ranges = line.split(',')
+    var elf1 = ranges[0].split('-')
+    var elf2 = ranges[1].split('-')
+    if(parseInt(elf1[0]) <= parseInt(elf2[1]) && parseInt(elf2[0]) <= parseInt(elf1[1])) {total++ }
+
+});
+console.log(total)
